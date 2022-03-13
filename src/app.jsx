8ed571b4684b1e61807c3c329fb8f1b410629796
pooh <cktjdgus45@naver.com>
2022-03-13 16:59:11 +0900

    jest setup & add

--- src/app.jsx
+++ src/app.jsx
@@ -2,9 +2,27 @@ import './app.css';
 
 function App() {
   return (
-    <>
-      clean
-    </>
+    <div className='calculator-grid'>
+      <div className="output">
+        <div className="previous-operand"></div>
+        <div className="current-operand"></div>
+      </div>
+      <button className='span-two'>AC</button>
+      <button>DEL</button>
+      <button>%</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className='span-wro'>=</button>
+    </div>
   )
 }
 
